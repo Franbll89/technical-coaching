a36079c86dce37d4a5311c856dce657b33fa4230
Francesco Belli <francesco.belli@guccidigital.com>
2024-01-31 15:23:24 +0100

    Add method return sum of comma separeted numbers

--- string_calculator/string_calculator.js
+++ string_calculator/string_calculator.js
@@ -1,6 +1,15 @@
 'use strict';
 
 class StringCalculator {
+    add(inputString) {
+        if (!inputString) {
+            return 0;
+        }
+
+        return inputString.split(',')
+            .map(s => parseInt(s))
+            .reduce((acc, v) => acc + v, 0)
+    }
 }
 
 module.exports = StringCalculator
--- string_calculator/string_calculator_test.js
+++ string_calculator/string_calculator_test.js
@@ -2,6 +2,20 @@
 
 const StringCalculator = require('./string_calculator.js')
 
-test('test runner is set up correctly', () => {
-    expect(0).toBe(1);
+test('empty string sums up to 0', () => {
+    expect(new StringCalculator().add('')).toBe(0);
+});
+
+test('A single number returns the value', () => {
+    expect(new StringCalculator().add('1')).toBe(1);
+    expect(new StringCalculator().add('2')).toBe(2);
+});
+
+test('Two numbers, comma delimited, returns the sum', () =>{
+    expect(new StringCalculator().add('1,2')).toBe(3);
+    expect(new StringCalculator().add('10,20')).toBe(30);
+});
+
+test('Three numbers, comma delimited, returns the sum', () =>{
+    expect(new StringCalculator().add('1,2,3')).toBe(6);
 });
